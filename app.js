1a3aec46ae3a41dafb9d5b41d276c2ba5345c693
12345120 <hehe9343@gmail.com>
2022-04-12 02:59:39 -0400

    ADD: basic UI (incomplete) for requests
    * added js for interactions (incomplete: option button clicks)
    * added css styling
    * added app.js for server for ui

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
-// for serving views 
+const express = require("express");
+const path = require("path");
+
+const app = express();
+
+app.use("/public", express.static("public"));
+
+app.get("/", async (req, res) => {
+  res.sendFile(path.join(__dirname, "/views", "index.html"));
+});
+
+app.listen(5000);
+console.log("main server on 5000!");
--- public/javascripts/fields.js
+++ public/javascripts/fields.js
@@ -1,10 +1,71 @@
-$(".add-btn").click(function (e) {
-  var ul = document.getElementsByClassName("query-params-list");
-  var li = document.createElement("li");
-  li.innerHTML = `
-    <div></div>
-  `;
-  
-  li.appendChild(document.createTextNode("Four"));
-  ul.appendChild(li);
+const requestMethods = {
+  GET: "GET",
+  POST: "POST",
+};
+
+let currentRequestMethod = requestMethods.GET;
+let optionBtns = null;
+
+function setElementText(element, text) {
+  element.textContent = text;
+}
+
+function toggleActiveElement(element) {
+  element.classList.toggle("active");
+}
+
+function deactivateElement(element) {
+  element.classList.remove("active");
+}
+
+function changeCurrReqMethod(newMethod) {
+  if (newMethod == "GET") {
+    currentRequestMethod = requestMethods.GET;
+  } else {
+    currentRequestMethod = requestMethods.POST;
+  }
+}
+
+function showQueryParams(e) {}
+
+function showHeaders(e) {}
+
+function showJSON(e) {}
+
+$(".dropdown-main-btn").click(function (e) {
+  let dropdownArea = document.querySelector(".dropdown-area");
+  toggleActiveElement(dropdownArea);
+});
+
+$(".dropdown-area .dropdown-btn").click(function (e) {
+  let chosenRequestMethod = e.target.textContent;
+  changeCurrReqMethod(chosenRequestMethod);
+});
+
+$(".btn").click(function (e) {
+  for (btn of optionBtns) {
+    if (btn == e.target) {
+      toggleActiveElement(e.target);
+    } else {
+      deactivateElement(btn);
+    }
+  }
+});
+
+$(".query-params-btn").click(function (e) {});
+
+$(".headers-btn").click(function (e) {});
+
+$(".json-btn").click(function (e) {});
+
+$(document).ready(function (e) {
+  let dropdownButtonText = document.querySelector(".dropdown-main-btn-text");
+  let dropdownArea = document.querySelector(".dropdown-area");
+
+  // set default text for dropdown button
+  let defaultText = dropdownArea.children[0].textContent;
+  setElementText(dropdownButtonText, defaultText);
+
+  // populate optionBtns
+  optionBtns = document.querySelectorAll(".btns-list .btn");
 });
